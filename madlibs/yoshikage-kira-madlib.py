55f0123702048f97a66aaed1fddbb8ba033888f5
ShanZeff <18shanzeff4@gmail.com>
2021-07-15 16:17:31 +0800

    finishing up

--- madlibs/yoshikage-kira-madlib.py
+++ madlibs/yoshikage-kira-madlib.py
@@ -1,19 +1,51 @@
 name1 = input("Name: ")
 name2 = input("Name: ")
 number1 = input("Number: ")
+number2 = input("Number: ")
+number3 = input("Number: ")
 noun1 = input("Noun: ")
 noun2 = input("Noun: ")
 noun3 = input("Noun: ")
+noun4 = input("Noun: ")
+noun5 = input("Noun: ")
+noun6 = input("Noun: ")
+noun7 = input("Noun: ")
+noun8 = input("Noun: ")
+noun9 = input("Noun: ")
+noun10 = input("Noun: ")
+noun11 = input("Noun: ")
+noun12 = input("Noun: ")
 direction1 = input("Direction: ")
 place1 = input("Place: ")
 past_verb1 = input("Past verb: ")
 verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+verb3 = input("Verb: ")
+verb4 = input("Verb: ")
+verb5 = input("Verb: ")
+verb6 = input("Verb: ")
+verb7 = input("Verb: ")
+verb8 = input("Verb: ")
+verb_ing1 = input("Verb ends with ing: ")
 job1 = input("Job: ")
-quantity_of_time = input("Quantity of time: ")
+quantity_of_time1 = input("Quantity of time: ")
+quantity_of_time2 = input("Quantity of time: ")
+quantity_of_time3 = input("Quantity of time: ")
 time1 = input("Time: ")
+time2 = input("Time: ")
+liquid_holder = input("Liquid holder: ")
+adj1 = input("Adjective: ")
+adj2 = input("Adjective: ")
+liquid1 = input("Liquid: ")
+organism1 = input("Organism: ")
 
 madlibs = f"My name is {name1}. I'm {number1} years old. My {noun1} is in the {direction1} section of {place1}, where all the {noun2} are, \
-and I am not {past_verb1}. I {verb1} as {job1} for the {name2} department stores, and I get {noun3} every {quantity_of_time} by {time1} PM \
-at the latest."
+and I am not {past_verb1}. I {verb1} as {job1} for the {name2} department stores, and I get {noun3} every {quantity_of_time1} by {time1} PM \
+at the latest. I don't {verb2}, but I occasionally {verb3}. I'm in {noun4} by {time2} PM, and make sure I get {number2} {quantity_of_time2} \
+of sleep, no matter what. After having a {liquid_holder} of {adj1} {liquid1} and doing about {number3} {quantity_of_time3} of {verb_ing1} \
+before going to bed, I usually have no problems {verb_ing1} until morning. Just like as {organism1}, I wake up without any {noun5} or \
+{noun6} in the morning, I was told there was no {noun7} at my last check-up. I'm trying to explain that I'm a {noun8} who whishes to {verb4} a \
+very {adj2} {noun9}. I take care not to {verb5} myself with any {noun10}, like winning or losing, that would cause me to lose {noun11} at night. \
+That is how I {verb6} with society, and I know that is what brings me {noun12}. Although, if I were to {verb7} I woudln't {verb8} to anyone."
 
 print(madlibs)
