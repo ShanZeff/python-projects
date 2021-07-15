6cf36b9a4e6e8c1c5c8c3d0bf0d1d8883312e303
ShanZeff <18shanzeff4@gmail.com>
2021-07-15 15:27:17 +0800

    update

---
+++ madlibs/yoshikage-kira-madlib.py
@@ -0,0 +1,19 @@
+name1 = input("Name: ")
+name2 = input("Name: ")
+number1 = input("Number: ")
+noun1 = input("Noun: ")
+noun2 = input("Noun: ")
+noun3 = input("Noun: ")
+direction1 = input("Direction: ")
+place1 = input("Place: ")
+past_verb1 = input("Past verb: ")
+verb1 = input("Verb: ")
+job1 = input("Job: ")
+quantity_of_time = input("Quantity of time: ")
+time1 = input("Time: ")
+
+madlibs = f"My name is {name1}. I'm {number1} years old. My {noun1} is in the {direction1} section of {place1}, where all the {noun2} are, \
+and I am not {past_verb1}. I {verb1} as {job1} for the {name2} department stores, and I get {noun3} every {quantity_of_time} by {time1} PM \
+at the latest."
+
+print(madlibs)
