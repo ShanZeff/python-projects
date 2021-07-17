653251a2af306d241229dd86a80e7e8834bb4700
ShanZeff <18shanzeff4@gmail.com>
2021-07-17 22:34:13 +0800

    input

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -3,5 +3,28 @@ from words import words
 from hangman_visual import lives_visual_dict
 import string
 
-print(lives_visual_dict[0])
-print(lives_visual_dict[3])
+def get_valid_word(words):
+    word = random.choice(words) # randomly chooses smth from the list
+    while "-" in word or " " in word:
+        word = random.choice(words)
+    
+    return word.upper()
+
+def hangman():
+    word = get_valid_word(words)
+    word_letters = set(word)    # letters in the word
+    alphabet = set(string.ascii_uppercase)
+    used_letters = set()        # what the user has guessed
+
+    # getting user input
+    user_letter = input("Guess a letter: ").upper()
+    if user_letter in alphabet - used_letters:
+        used_letters.add(user_letter)
+        if user_letter in word_letters:
+            word_letters.remove(user_letter)
+
+    elif user_letter in used_letters:
+        print("You have already used that character. Please try again.")
+
+    else:
+        print("Invalid character. Please try again.")
