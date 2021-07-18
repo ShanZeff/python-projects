3fc904b0a4934bce1a0c1549fdf677a985b5da0d
ShanZeff <18shanzeff4@gmail.com>
2021-07-18 10:12:55 +0800

    lives

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -18,11 +18,13 @@ def hangman():
     alphabet = set(string.ascii_uppercase)
     used_letters = set()        # what the user has guessed
 
+    lives = 7
+
     # getting user input
-    while len(word_letters) > 0:
+    while len(word_letters) > 0 and lives > 0:
         # letters used
         # " ".join(["a", "b", "cd"]) --> "a b cd"
-        print("You have used these letters: ", " ".join(used_letters))
+        print("You have", lives, "lives left and you have used these letters: ", " ".join(used_letters))
 
         # what the current word is (i.e W - R D)
         word_list = [letter if letter in used_letters else "-" for letter in word]
@@ -34,13 +36,22 @@ def hangman():
             if user_letter in word_letters:
                 word_letters.remove(user_letter)
 
+            else:
+                lives = lives - 1   # takes away a life if wrong
+                print("Letter is not in the word.")       
+
         elif user_letter in used_letters:
             print("You have already used that character. Please try again.")
 
         else:
             print("Invalid character. Please try again.")
 
-    # gets here when len(word_letters) == 0
+    # gets here when len(word_letters) == 0 OR when lives == 0
+    if lives == 0:
+        print("You died, sorry. The word was", word)
+
+    else:
+        print("You have guessed the word", word, "correctlly!!")
 
 if __name__ == '__main__':      # bruhhh i forgot to call
     hangman()
