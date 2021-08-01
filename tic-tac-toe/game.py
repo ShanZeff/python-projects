6b691662a6bdce77250197c37dc99707bf561a6a
ShanZeff <18shanzeff4@gmail.com>
2021-08-01 15:10:01 +0800

    finished

--- tic-tac-toe/game.py
+++ tic-tac-toe/game.py
@@ -1,3 +1,6 @@
+import time
+from player import HumanPlayer, RandomComputerPlayer
+
 class TicTacToe:
     def __init__(self):
         self.board = [' ' for _ in range(9)]    # we will use a single list to rep. the 3x3 board
@@ -33,8 +36,8 @@ class TicTacToe:
     def make_move(self, square, letter):
         # if invalid move, then make the move (assign square to letter)
         # then return true, if invalid, return false
-        if self.board(square) == ' ':
-            self.board(square) = letter
+        if self.board[square] == ' ':
+            self.board[square] = letter
             if self.winner(square, letter):
                 self.current_winner = letter
             return True
@@ -53,6 +56,21 @@ class TicTacToe:
         column = [self.board[col_ind+i*3] for i in range(3)]
         if all([spot == letter for spot in column]):
             return True
+
+        # check diagonals
+        # but only if the square is an even number [0, 2, 4, 6, 8]
+        # these are the only moves possible to win a diagonal
+        if square % 2 == 0:
+            diagonal1 = [self.board[i] for i in [0, 4, 8]]      # left to right diagonal
+            if all([spot == letter for spot in diagonal1]):
+                return True
+
+            diagonal2 = [self.board[i] for i in [2, 4, 6]]      # right to left diagonal
+            if all([spot == letter for spot in diagonal2]):
+                return True
+        
+        # if all of these fail
+        return False
 
 
 def play(game, x_player, o_player, print_game = True):
@@ -91,5 +109,14 @@ def play(game, x_player, o_player, print_game = True):
             # else:
             #     letter = 'X'
 
-            if print_game:
-                print('It\'s a tie!' )
+        # tiny break to make things a lil easier to read
+        time.sleep(0.8)
+
+    if print_game:
+        print('It\'s a tie!' )
+
+if __name__ == '__main__':
+    x_player = HumanPlayer('X')
+    o_player = RandomComputerPlayer('O')
+    t = TicTacToe()
+    play(t, x_player, o_player, print_game = True)
