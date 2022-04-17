36ca980c9b5c39169b987d7e97c1c9f531b025a6
ShanZeff <18shanzeff4@gmail.com>
2022-04-17 23:17:14 +0800

    images not correct size

--- balloon-flight/balloon.py
+++ balloon-flight/balloon.py
@@ -4,6 +4,9 @@ import pygame
 # Initialise the pygame
 pygame.init()
 
+# background
+background = pygame.image.load('background.png')
+
 # create the screen
 screen = pygame.display.set_mode((800, 600))
 
@@ -12,9 +15,36 @@ pygame.display.set_caption("Balloon Flight")
 icon = pygame.image.load('air-hot-balloon.png')
 pygame.display.set_icon(icon)
 
+# player
+playerImg = pygame.image.load('balloon.png')
+playerX = 370
+playerY = 300
+
+# test
+houseImg = pygame.image.load('house.png')
+houseX = 100
+houseY = 300
+
+treeImg = pygame.image.load('tree.png')
+treeX = 200
+treeY = 200
+
+
+def player():
+    screen.blit(playerImg, (playerX, playerY))
+    screen.blit(houseImg, (houseX, houseY))
+    screen.blit(treeImg, (treeX, treeY))
+
+
 # Game loop
 running = True
 while running:
+
+    # background image
+    screen.blit(background, (0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    player()
+    pygame.display.update()
